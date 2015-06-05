1b13df0c17a8905d8d5baa42f0dc84086c8c3740
GNR solution PVT.LTD <GNR@GNRs-MacBook-Pro.local>
2015-06-05 19:57:18 +0530

    Initial Commit

---
+++ RefreshControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RefreshControl
+//
+//  Created by GNR solution PVT.LTD on 05/06/15.
+//  Copyright (c) 2015 Harshavardhan Edke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RefreshControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RefreshControl
+//
+//  Created by GNR solution PVT.LTD on 05/06/15.
+//  Copyright (c) 2015 Harshavardhan Edke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
